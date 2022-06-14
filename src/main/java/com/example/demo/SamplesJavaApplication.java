40ad8c34af0275ce1a719212e7908989df446a47
gouravkrosx <gouravgreatkr@gmail.com>
2022-06-14 12:28:41 +0530

    fix: routes naming

--- src/main/java/com/example/demo/SamplesJavaApplication.java
+++ src/main/java/com/example/demo/SamplesJavaApplication.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication(scanBasePackages = {"com.example.demo","io.keploy"})
 @SpringBootApplication
 public class SamplesJavaApplication {
 
 	public static void main(String[] args) {
+//		KeployInstance ki = KeployInstance.getInstance();
 		SpringApplication.run(SamplesJavaApplication.class, args);
 	}
 
 }
+
--- src/main/java/com/example/demo/controller/FresherController.java
+++ src/main/java/com/example/demo/controller/FresherController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/")
 public class FresherController {
 
     @Autowired
