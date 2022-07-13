d447376427c551f551120af2ac5d7ff6afb58e34
gouravkrosx <gouravgreatkr@gmail.com>
2022-07-13 10:33:06 +0530

    refactor imports

--- src/main/java/com/example/demo/SamplesJavaApplication.java
+++ src/main/java/com/example/demo/SamplesJavaApplication.java
@@ -1,29 +1,25 @@
 package com.example.demo;
 
-//import com.gkrosx.filter_dependency.middleware;
-
 import io.github.cdimascio.dotenv.Dotenv;
-import io.keploy.grpc.GrpcClient;
+import io.keploy.client.GrpcClient;
 import io.keploy.regression.KeployInstance;
 import io.keploy.regression.keploy.AppConfig;
 import io.keploy.regression.keploy.Config;
 import io.keploy.regression.keploy.Keploy;
 import io.keploy.regression.keploy.ServerConfig;
 import io.keploy.regression.mode;
-import io.keploy.servlet.middleware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.example.demo", "io.keploy.servlet"})
 //@SpringBootApplication
-//@SpringBootApplication(scanBasePackages = {"com.example.demo","com.gkrosx.filter_dependency"})
 public class SamplesJavaApplication {
     public static void main(String[] args) {
         KeployInstance ki = KeployInstance.getInstance();
         Keploy kp = new Keploy();
         Config cfg = new Config();
         AppConfig appConfig = new AppConfig();
-        appConfig.setName("samples_java");
+        appConfig.setName("samples_java1");
         appConfig.setPort("8090");
         ServerConfig serverConfig = new ServerConfig();
         serverConfig.setURL("http://localhost:8081/api");
