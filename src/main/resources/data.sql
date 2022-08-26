a888170ebfaf7353db4f4e6b4a4683a97094c597
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2022-08-26 16:16:35 +0530

    refactor: refactor data.sql and readme file (#7)
    * refactor: refactor data.sql and readme file
    
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>
    
    * refactor(pom.xml): enable skip test true in pom.xml file
    
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>
    
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>
    Signed-off-by: Rema <reem.mohamed00@eng-st.cu.edu.eg>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,17 @@
+
+delete from employees;
+delete from freshers;
+
 insert into employees
-values (1, 'Gourav', 'Kumar', 'gkrosx@gmail.com', 16);
+values (1, 'Gourav', 'Kumar', 'gkrosx@gmail.com', 1661500330);
 insert into employees
-values (2, 'Sarthak', 'Sharma', 'ss@gmail.com', 72);
+values (2, 'Sarthak', 'Sharma', 'ss@gmail.com', 1661500450);
 insert into employees
-values (3, 'Ritik', 'Jain', 'rj@gmail.com', 993);
+values (3, 'Ritik', 'Jain', 'rj@gmail.com', 1661500810);
 insert into employees
-values (4, 'Shashank', 'Pandey', 'sp@gmail.com', 55);
+values (4, 'Shashank', 'Pandey', 'sp@gmail.com', 1661500960);
 insert into employees
-values (5, 'Akshit', 'Taneja', 'at@gmail.com', 34);
+values (5, 'Akshit', 'Taneja', 'at@gmail.com', 1661501190);
 
 insert into freshers
 values (1, 2022, 'GouravKumar');
