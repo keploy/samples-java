78fd43957eff4bb93952ad7bbd13467ae4f6e0bd
Sarthak Shyngle <50234097+Sarthak160@users.noreply.github.com>
2022-09-08 16:13:54 +0530

    refactor: remove uncovered code for testcoverage
    Signed-off-by: Sarthak Shyngle <50234097+Sarthak160@users.noreply.github.com>

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -22,12 +22,6 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    //get employees
-    @GetMapping("employees")
-    public List<Employee> getAllEmployee() {
-        return this.employeeRepository.findAll();
-    }
-
     //get employee by id
     @GetMapping("employees/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId) throws ResourceNotFoundException {
@@ -40,29 +34,5 @@ public class EmployeeController {
     public Employee createEmployee(@RequestBody Employee employee) {
         employee.setTimestamp(Instant.now().getEpochSecond());
         return this.employeeRepository.save(employee);
-    }
-
-    //update employee
-    @PutMapping("employees/{id}")
-    public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Long employeeId, @Valid @RequestBody Employee employeeDetails) throws ResourceNotFoundException {
-
-        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
-        employee.setEmail(employeeDetails.getEmail());
-        employee.setFirstName(employeeDetails.getFirstName());
-        employee.setLastName(employeeDetails.getLastName());
-        employee.setTimestamp(Instant.now().getEpochSecond());
-
-        return ResponseEntity.ok(this.employeeRepository.save(employee));
-    }
-
-    //delete employee
-    @DeleteMapping("employees/{id}")
-    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId) throws ResourceNotFoundException {
-        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
-        this.employeeRepository.delete(employee);
-        Map<String, Boolean> response = new HashMap<>();
-        response.put("deleted", Boolean.TRUE);
-
-        return response;
     }
 }
