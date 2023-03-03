f0745f25a84363e25f6de037a7b14bf3a2af4f0e
Rema <reem.mohamed00@eng-st.cu.edu.eg>
2023-03-03 11:54:12 +0200

    jwt with keploy done sign off
    Signed-off-by: Rema <reem.mohamed00@eng-st.cu.edu.eg>

--- src/test/java/SamplesJavaApplication_Test.java
+++ src/test/java/SamplesJavaApplication_Test.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import java.time.Instant;
 import java.util.concurrent.CountDownLatch;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class SamplesJavaApplication_Test {
 
@@ -35,4 +37,36 @@ public class SamplesJavaApplication_Test {
         countDownLatch.await();
         assertTrue(AssertKTests.result());
     }
+
+//    @Test
+//    public void testCreateEmployee() {
+//        System.out.println("testing createEmployee controller");
+//        // Create a new Employee object for the request body
+////        Employee requestEmployee = new Employee();
+////        requestEmployee.setFirstName("John");
+////        requestEmployee.setLastName("Doe");
+////        requestEmployee.setEmail("john.doe@example.com");
+////
+////        // Create a mock Employee object to return from the repository
+////        Employee savedEmployee = new Employee();
+////        savedEmployee.setId(1L);
+////        savedEmployee.setFirstName("John");
+////        savedEmployee.setLastName("Doe");
+////        savedEmployee.setEmail("john.doe@example.com");
+////        savedEmployee.setTimestamp(Instant.now().getEpochSecond());
+////        when(employeeRepository.save(requestEmployee)).thenReturn(savedEmployee);
+////
+////        // Call the createEmployee method with the request Employee object
+////        Employee resultEmployee = employeeController.createEmployee(requestEmployee);
+////
+////        // Verify that the repository save method was called with the request Employee object
+////        ArgumentCaptor<Employee> captor = ArgumentCaptor.forClass(Employee.class);
+////        verify(employeeRepository).save(captor.capture());
+////        assertEquals(requestEmployee, captor.getValue());
+//
+//        // Verify that the returned Employee object matches the mock object returned from the repository
+//        assertTrue(true);
+//        System.out.println("Done executing this unit test case (createEmployee)");
+//    }
+
 }
