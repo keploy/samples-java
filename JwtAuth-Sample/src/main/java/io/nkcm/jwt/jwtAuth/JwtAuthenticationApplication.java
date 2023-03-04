77492570f0158ec33c4b7b5934eba5c3d4b19700
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-03-04 17:33:37 +0545

    dependency fixed
    Signed-off-by: Kshitiz-Mhto <nkcmcdhy@gmail.com>

--- JwtAuth-Sample/src/main/java/io/nkcm/jwt/jwtAuth/JwtAuthenticationApplication.java
+++ JwtAuth-Sample/src/main/java/io/nkcm/jwt/jwtAuth/JwtAuthenticationApplication.java
@@ -1,9 +1,12 @@
 package io.nkcm.jwt.jwtAuth;
 
+import io.keploy.servlet.KeployMiddleware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(KeployMiddleware.class)
 public class JwtAuthenticationApplication {
 
 	public static void main(String[] args) {
