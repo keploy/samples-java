a7ae851930075fb4877441a8743dd59eb320e1b3
charankamarapu <kamarapucharan@gmail.com>
2024-09-15 13:18:52 +0530

    bug: mongo auth fails
    Signed-off-by: charankamarapu <kamarapucharan@gmail.com>

--- spring-boot-mongo/src/main/java/com/example/potionsapi/exception/ResourceNotFoundException.java
+++ spring-boot-mongo/src/main/java/com/example/potionsapi/exception/ResourceNotFoundException.java
@@ -2,12 +2,9 @@ package com.example.potionsapi.exception;
 
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.io.Serial;
-
 @ResponseStatus
 public class ResourceNotFoundException extends RuntimeException {
 
-    @Serial
     private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(String message) {
