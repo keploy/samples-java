d84905b9e1265bec2d849c001793b53ef852f5d6
AhmedLotfy02 <ahmadlotfygamersfield@gmail.com>
2025-03-05 17:03:51 +0200

    [add]: sample java app with xml resposne
    Signed-off-by: AhmedLotfy02 <ahmadlotfygamersfield@gmail.com>

---
+++ spring-boot-xml/naive-spring-boot/src/main/java/com/example/XML/entities/User.java
@@ -0,0 +1,46 @@
+package com.example.XML.entities;
+
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "User")
+public class User {
+    @XmlElement(name = "name")
+    private String name;
+
+    @XmlElement(name = "age")
+    private int age;
+
+    @XmlElement(name = "mobile")
+    private String mobile;
+    // Constructors, getters, and setters
+    public User() {}
+
+    public User(String name, int age, String mobile) {
+        this.name = name;
+        this.age = age;
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getMobile() {
+        return mobile;
+    }
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
