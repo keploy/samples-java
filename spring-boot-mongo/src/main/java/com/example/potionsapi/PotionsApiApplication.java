8beddcba8a60f5ceb79b5388efd98756490fdeec
Harsh Kumar <harshxkumar7@gmail.com>
2025-06-19 06:14:56 +0530

    Added basic comment to PotionsApiApplication.java just to start the journey

--- spring-boot-mongo/src/main/java/com/example/potionsapi/PotionsApiApplication.java
+++ spring-boot-mongo/src/main/java/com/example/potionsapi/PotionsApiApplication.java
@@ -3,6 +3,11 @@ package com.example.potionsapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is the main class for the Potions API Spring Boot application
+ * It serves as the entry point for starting the application
+ */
+
 @SpringBootApplication
 public class PotionsApiApplication {
 
