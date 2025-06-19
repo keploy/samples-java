4d3c94cc99b5c434f763ebd2610b537b743d2c06
Sudhanshu137 <ssudhanshu137@gmail.com>
2025-06-19 14:57:20 +0530

    Added HelloController to employee-manager sample for Keploy Fellowship

---
+++ employee-manager/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Sudhanshu 👋 - Keploy API Fellow!";
+    }
+}
